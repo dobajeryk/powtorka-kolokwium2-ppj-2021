421593d8bf1907fabf0a85578a63c9228cf33e21
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-18 13:08:06 +0100

    Dodano treść trzeciego zadania (łatwe)

--- src/latwe/Zadanie3.java
+++ src/latwe/Zadanie3.java
@@ -2,5 +2,14 @@ package latwe;
 
 public class Zadanie3 {
     public static void main(String[] args) {
+        /*
+        Dana jest tablica int
+        int [] tab = {100, 97, 3, 12, 12321, 2324, 1, 1};
+        napisz program, który utworzy dwie tablice.
+        Pierwsza z nich będzie przechowywać liczby parzyste natomiast druga liczby nieparzyste.
+        Uwaga!
+        Rozmiar utworzonych tablic powinien być taki, ile jest elementów w tej tablicy. Niedopuszczalne jest stworzenie
+        większej tablic niż potrzeba!
+         */
     }
 }
