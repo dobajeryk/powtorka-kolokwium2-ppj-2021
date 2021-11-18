615f610b22e4b3f577e117018809ba18b92fdcf4
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-18 11:58:35 +0100

    Dodano treść pierwszego zadania (łatwe)

---
+++ src/latwe/Zadanie1.java
@@ -0,0 +1,16 @@
+package latwe;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        /*
+        Dana jest tablica char
+        char doOdwrocenia [] = {'a', 'b', 'c', 'd'};
+        twoim zadaniem jest odwrócenie elementów w tej tablicy (pierwszy element na ostatnim miejscu, ostatni na
+        pierwszym itd.). Finalnie nasza tablica powinna wyglądać tak:
+        char doOdwrocenia [] = {'d', 'c', 'b', 'a'};
+        Uwaga!
+        W zadaniu nie możesz użyć żadnej dodatkowej tablicy!
+        */
+        char [] doOdwrocenia = {'a', 'b', 'c', 'd'};
+    }
+}
