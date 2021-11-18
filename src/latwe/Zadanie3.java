581cf803799be29b34ddbea99bde0b5cc8bdf335
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-18 13:19:17 +0100

    Rozwiązanie trzeciego zadania (łatwe)

--- src/latwe/Zadanie3.java
+++ src/latwe/Zadanie3.java
@@ -11,5 +11,37 @@ public class Zadanie3 {
         Rozmiar utworzonych tablic powinien być taki, ile jest elementów w tej tablicy. Niedopuszczalne jest stworzenie
         większej tablic niż potrzeba!
          */
+        int [] tab = {100, 97, 3, 12, 12321, 2324, 1, 1};
+        // zliczenie ile jest liczb parzystych a ile nieparzystych
+        int parzyste = 0;
+        int nieParzyste = 0;
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] % 2 == 0) {
+                parzyste++;
+            } else {
+                nieParzyste++;
+            }
+        }
+        // utworzenie tablic o odpowiednim rozmiarze
+        int [] parzysteTab = new int[parzyste];
+        int [] nieParzysteTab = new int[nieParzyste];
+        // dodanie liczb z tab do odpowiedniej tablicy
+        for (int i = 0, licznikParz = 0, licznikNieParz = 0; i < tab.length; i++) {
+            if (tab[i] % 2 == 0) {
+                parzysteTab[licznikParz++] = tab[i];
+            } else {
+                nieParzysteTab[licznikNieParz++] = tab[i];
+            }
+        }
+        // wypisanie tablic
+        System.out.println("Tablica z liczbami parzystymi");
+        for (int i = 0; i < parzysteTab.length; i++) {
+            System.out.print(parzysteTab[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Tablica z liczbami nieparzystymi");
+        for (int i = 0; i < nieParzysteTab.length; i++) {
+            System.out.print(nieParzysteTab[i] + " ");
+        }
     }
 }
