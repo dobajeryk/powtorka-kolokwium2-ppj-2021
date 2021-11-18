43b8bbfcde872b72e3c8300bb2305d265290384f
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-18 13:03:40 +0100

    Dodano treść pierwszego zadania (trudniejsze)

---
+++ src/trudniejsze/Zadanie1.java
@@ -0,0 +1,10 @@
+package trudniejsze;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        /*
+        Napisz program, który zamieni liczbę z systemu dziesiętnego na system ósemkowy.
+        Np. dla liczby 1271 (dziesiętnej) powinen wypisać 2370 (reprezentacja tej liczby w systemie ósemkowym).
+         */
+    }
+}
