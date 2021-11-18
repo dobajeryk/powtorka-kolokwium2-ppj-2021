1a91a7402eacc5514d8b5e692a825f0628d886c9
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-18 13:05:16 +0100

    Zadanie3(łatwiejsze) -> Zadanie2(trudniejsze)

--- src/latwe/Zadanie3.java
+++ src/latwe/Zadanie3.java
@@ -2,14 +2,5 @@ package latwe;
 
 public class Zadanie3 {
     public static void main(String[] args) {
-        /*
-        Dana jest tablica
-        char znaki [] =  {'k', 'o', 'k', 'o', 's'};
-        Napisz program, który wypisz ile razy wystąpił każdy ze znaków.
-        Dla danego przykładu program powinen wypisać:
-        Znak k wystąpił 2 razy
-        Znak o wystąpił 2 razy
-        Znak t wystąpił 1 razy
-         */
     }
 }
---
+++ src/trudniejsze/Zadanie2.java
@@ -0,0 +1,15 @@
+package trudniejsze;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        /*
+        Dana jest tablica
+        char znaki [] =  {'k', 'o', 'k', 'o', 's'};
+        Napisz program, który wypisz ile razy wystąpił każdy ze znaków.
+        Dla danego przykładu program powinen wypisać:
+        Znak k wystąpił 2 razy
+        Znak o wystąpił 2 razy
+        Znak t wystąpił 1 razy
+         */
+    }
+}
