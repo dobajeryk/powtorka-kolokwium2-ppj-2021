7ba4390d6dbfb751cb6a0795cfe801a02f8b2060
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-18 13:21:57 +0100

    Rozwiązanie drugiego zadania (trudniejsze)

--- src/trudniejsze/Zadanie2.java
+++ src/trudniejsze/Zadanie2.java
@@ -11,5 +11,17 @@ public class Zadanie2 {
         Znak o wystąpił 2 razy
         Znak t wystąpił 1 razy
          */
+        char[] znaki = {'k', 'o', 'k', 'o', 's'};
+        // zliczenie ile razy wystapil dany znak
+        int[] tablicaPomocnicza = new int[128];
+        for (int i = 0; i < znaki.length; i++) {
+            int wartoscAscii = znaki[i];
+            tablicaPomocnicza[wartoscAscii] += 1;
+        }
+        for (int i = 0; i < tablicaPomocnicza.length; i++) {
+            if (tablicaPomocnicza[i] != 0) {
+                System.out.println("Znak: " + (char) i + " wystąpił : " + tablicaPomocnicza[i] + " razy");
+            }
+        }
     }
 }
