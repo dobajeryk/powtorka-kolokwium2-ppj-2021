92faae0cfdf6928cd0d1a099e54144887b21c9df
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-18 12:02:45 +0100

    Dodano treść drugiego zadania (łatwe)

---
+++ src/latwe/Zadanie2.java
@@ -0,0 +1,14 @@
+package latwe;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        /*
+        Dana jest tablica int
+        int [] tab = {1, 2, 3, 4, 4, 3, 2, 1};
+        Twoje zadanie polega na wypisaniu booleana (true lub false) w zależności od tego czy liczby w tej tablicy
+        są palindromem (https://pl.wikipedia.org/wiki/Palindrom) czy nie.
+        Co to palindrom w skrócie?
+        Wyrażenie brzmiące tak samo czytane od lewej do prawej i od prawej do lewej.
+         */
+    }
+}
