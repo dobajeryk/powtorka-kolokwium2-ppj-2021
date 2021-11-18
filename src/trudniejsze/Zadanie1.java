351d68f30effb5ccc65dabf5e432d651ccade451
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-18 13:34:02 +0100

    Aktualizacja treści pierwszego zadania (trudniejsze)

--- src/trudniejsze/Zadanie1.java
+++ src/trudniejsze/Zadanie1.java
@@ -4,7 +4,8 @@ public class Zadanie1 {
     public static void main(String[] args) {
         /*
         Napisz program, który zamieni liczbę z systemu dziesiętnego na system ósemkowy.
-        Np. dla liczby 1271 (dziesiętnej) powinen wypisać 2370 (reprezentacja tej liczby w systemie ósemkowym).
+        Liczbę zapisaną w systemie ósemkowym zapisz w tablicy typu char a następnie wypisz tablicę.
+        Np. dla liczby 1271 (dziesiętnej) powinen wypisać 2367 (reprezentacja tej liczby w systemie ósemkowym).
          */
     }
 }
